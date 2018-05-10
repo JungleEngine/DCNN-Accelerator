28bb3fc9fc03a2046fa6812abd436df1ecb2e328
mohamed ali <muhamedali166@gmail.com>
2018-05-10 22:19:41 +0200

    multiplication in accelerator tested.

--- multiplier.vhd
+++ multiplier.vhd
@@ -10,7 +10,9 @@ port (
 		-- The counter is the clock driven by the outer clock.
 		clk  : in std_logic;
 		start : in std_logic;
-		op1, op2 : in std_logic_vector(7 downto 0)
+		enable : in std_logic;
+		op1, op2 : in std_logic_vector(7 downto 0);
+		result : out std_logic_vector(15 downto 0)
 
 	);
 end entity multiplier;
@@ -21,34 +23,51 @@ architecture arch of multiplier is
 -- A = op1 + 9 zeros, S = 2's complement of op1 + 9 zeros.
 -- P = 8 zeros + op2 + 1 zero. 
 -- All are 17 bit signals.
-signal sig_a, sig_s, sig_p : std_logic_vector(16 downto 0):=(others=>'0');
+signal sig_a, sig_s, sig_p : std_logic_vector(16 downto 0);
+
+signal  sig_adder_a, sig_adder_b, sig_addition_out: std_logic_vector(16 downto 0);
+signal sig_carry_in : std_logic;
 
 begin
+  N_ADDER: entity work.NAdder generic map(n=>17)  port map 
+  (sig_adder_a, sig_adder_b, sig_carry_in,sig_addition_out);
 
+  	result <= sig_p(16 downto 1	);
+	process(clk, start, sig_addition_out, sig_adder_a, sig_adder_b, enable )
 
-
-	process(clk, start)
+	variable var_temp : std_logic_vector(16 downto 0):=(others=>'0');
 	begin
-		if(rising_edge(start)) then
+		if(enable ='1') then
+		--sig_addition_out<=var_addition_result;
+		if (start = '1' ) then
+			sig_adder_a <= ("000000000" & not op1);
+			sig_adder_b <= (others=>'0');
+			sig_carry_in <= '1';
 			sig_a <= (op1 & "000000000");
-			sig_s <= (std_logic_vector(unsigned(not op1 + 1)) & "000000000");
+			sig_s <= (sig_addition_out(7 downto 0) & "000000000");
 			sig_p <= ("00000000" & op2 & '0');
 		else 
 			if(rising_edge(clk)) then
 				if(sig_p(1 downto 0 )= "01") then 
-					sig_p <= (std_logic_vector(unsigned( sig_p + sig_a)));
-
+					sig_adder_a <= sig_p;
+					sig_adder_b <= sig_a;
+					sig_carry_in <= '0';
 				elsif (sig_p(1 downto 0 )= "10") then
-					sig_p <= (std_logic_vector(unsigned( sig_p + sig_s)));
-
+					sig_adder_a <= sig_p;
+					sig_adder_b <= sig_s;
+					sig_carry_in <= '0';
 				else 
-					sig_p <= sig_p;
-
+					sig_adder_a <= sig_p;
+					sig_adder_b <= (others=>'0');
+					sig_carry_in <= '0';
 				end if;
-				sig_p <= ('0' & sig_p(15 downto 0));
+			elsif (falling_edge(clk)) then
+		   		sig_p <=( sig_addition_out(16) & sig_addition_out(16 downto 1));
+
 			end if;
-		
+
 		end if;
+	end if;
 
 	end process;
 end;
